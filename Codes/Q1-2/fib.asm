c7b2831960f67c70bb0bc5a5cf7bae7a9d3ad09b
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-25 23:12:04 +0530

    Update fib.asm

--- Codes/Q1-2/fib.asm
+++ Codes/Q1-2/fib.asm
@@ -5,43 +5,57 @@ section .data
     nl db 10
 
 section .bss
-    fib resd 10  ; Buffer to store the ASCII representation of the Fibonacci number
+    fib resb 16  ; Buffer to store the ASCII representation of the Fibonacci number
 
 section .text
     global _start
 
 _start:
     ; Programmer-defined constant for the value of n
-    mov eax, 10  ; Change this value to find a different Fibonacci number
-
-    ; Initialize variables to hold the last two Fibonacci numbers
-    mov ebx, 0  ; F(n-2)
-    mov ecx, 1  ; F(n-1)
+    mov ecx, 10  ; Change this value to find the nth Fibonacci number
 
     ; Check if n is 0 or 1 (special cases)
-    cmp eax, 0
+    cmp ecx, 0
     je .fibonacci_done
-    cmp eax, 1
+    cmp ecx, 1
     je .fibonacci_done
+
+    ; Initialize variables to hold the last two Fibonacci numbers
+    mov eax, 0  ; F(n-2)
+    mov ebx, 1  ; F(n-1)
 
     ; Loop to calculate the nth Fibonacci number
 .fibonacci_loop:
     ; Calculate F(n) = F(n-1) + F(n-2)
-    add ebx, ecx
+    add eax, ebx
 
     ; Update variables for the next iteration
-    mov ecx, ebx  ; F(n-1) = F(n)
-    mov ebx, eax  ; F(n-2) = n
+    xchg eax, ebx  ; F(n-2) = F(n-1), F(n-1) = F(n)
 
     ; Decrement the counter
-    dec eax
+    dec ecx
 
     ; Check if we have reached the second Fibonacci number (F(1))
-    cmp eax, 1
-    jge .fibonacci_loop
+    jnz .fibonacci_loop
 
 .fibonacci_done:
-    ; The result (the nth Fibonacci number) is now in the ecx register
+    ; The result (the nth Fibonacci number) is now in the eax register
+
+    ; Convert the result (F(n)) to a string
+    xor edi, edi     ; Clear EDI (used for buffer index)
+    mov byte [fib + edi], '0'  ; Initialize the buffer with '0' (in case it's a single-digit number)
+    mov esi, eax     ; Copy the Fibonacci number to ESI
+
+.convert_to_string:
+    mov eax, esi     ; Restore the Fibonacci number from ESI
+    xor edx, edx     ; Clear EDX before the division
+    mov ebx, 10      ; Divisor (10)
+    div ebx          ; Divide EAX by 10, quotient in EAX, remainder in EDX
+    add dl, '0'      ; Convert the digit to ASCII character
+    inc edi          ; Move to the next position in the buffer
+    mov byte [fib + edi], dl  ; Store the ASCII digit in the fib buffer
+    test eax, eax    ; Check if EAX is zero (F(n) has been completely converted)
+    jnz .convert_to_string
 
     ; Display the output message
     mov eax, 4        ; syscall for sys_write
@@ -50,31 +64,21 @@ _start:
     mov edx, out_msg_len
     int 0x80          ; make syscall
 
-    ; Convert the result (F(n)) to a string
-    mov edi, 10       ; Divisor (10)
-    xor esi, esi      ; Counter for the number of digits in the Fibonacci number
-
-.convert_to_string:
-    xor edx, edx      ; Clear EDX before the division
-    div edi           ; Divide EAX by 10, quotient in EAX, remainder in EDX
-    add dl, '0'       ; Convert the digit to ASCII character
-    mov [fib + esi], dl ; Store the ASCII digit in the fib buffer
-    inc esi           ; Increment the digit counter
-    test eax, eax     ; Check if EAX is zero (F(n) has been completely converted)
-    jnz .convert_to_string
+    ; Move EDI to the end of the buffer (pointing to the last digit)
+    lea edi, [fib + edi]
 
     ; Display the Fibonacci number digit by digit
 .display_loop:
-    dec esi           ; Move the digit counter back to the last digit
     mov eax, 4        ; syscall for sys_write
     mov ebx, 1        ; file descriptor 1 (stdout)
-    lea ecx, [fib + esi] ; pointer to the digit character
+    mov ecx, edi      ; pointer to the digit character
     mov edx, 1        ; number of bytes to write (1 character)
     int 0x80          ; make syscall
 
-    ; Continue displaying the digits until all are printed
-    test esi, esi
-    jnz .display_loop
+    ; Move to the previous position in the buffer
+    dec edi
+    cmp edi, fib
+    jge .display_loop
 
     ; Display a newline character
     mov eax, 4        ; syscall for sys_write
