7f4366130faa44c68e083934a28d520b6127d325
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-26 01:15:03 +0530

    Update itoa.asm

--- Codes/Q1-2/itoa.asm
+++ Codes/Q1-2/itoa.asm
@@ -1,67 +1,59 @@
+section .data
+    output_buffer db 11 ; Allocate 11 bytes for the output buffer (10 digits + null terminator)
+
 section .bss
-    num resb 10         ; Buffer to store ASCII representation of the number
-    len equ $ - num     ; Length of the buffer (10 bytes)
+    num resd 1         ; Reserve 4 bytes (32 bits) to store the input integer
 
 section .text
     global _start
 
 _start:
-    ; Read an integer from the user (assuming a 64-bit Linux system)
-    mov eax, 0          ; System call number for sys_read (0)
-    mov edi, 0          ; File descriptor 0 (stdin)
-    lea esi, [num]      ; Buffer to store the input
-    mov edx, len        ; Maximum number of bytes to read
-    syscall
+    ; Get the input integer (you can replace 42 with any desired integer)
+    mov eax, 42
+    mov [num], eax
 
-    ; Convert the ASCII input to an integer (using atoi function)
-    call ascii_to_int
-
-    ; Convert the integer to an ASCII string
+    ; Convert the integer to ASCII string
+    mov ebx, output_buffer   ; Set ebx to point to the output buffer
     call int_to_ascii
 
-    ; Print the ASCII string (assuming a 64-bit Linux system)
-    mov eax, 4          ; System call number for sys_write (4)
-    mov edi, 1          ; File descriptor 1 (stdout)
-    lea esi, [num]      ; Buffer containing the ASCII string
-    mov edx, dword [len] ; Length of the ASCII string (using 32-bit size)
-    syscall
+    ; Print the ASCII string
+    mov eax, 4               ; 'write' system call number
+    mov ebx, 1               ; file descriptor (1 = STDOUT)
+    mov ecx, output_buffer   ; pointer to the buffer
+    mov edx, 11              ; number of bytes to write (including null terminator)
+    int 0x80                 ; invoke the system call
 
     ; Exit the program
-    mov eax, 1          ; System call number for sys_exit (1)
-    xor edi, edi        ; Exit code 0
-    syscall
+    mov eax, 1               ; 'exit' system call number
+    xor ebx, ebx             ; exit code 0
+    int 0x80                 ; invoke the system call
 
-; Function to convert ASCII input to an integer (atoi)
-ascii_to_int:
-    xor eax, eax        ; Clear eax to store the result
-    xor ecx, ecx        ; Clear ecx (loop counter)
-.loop:
-    movzx edx, byte [esi + ecx] ; Load the next ASCII character
-    test edx, edx       ; Check for the null terminator (end of the string)
-    jz .done            ; If null terminator found, exit the loop
-    sub edx, '0'        ; Convert ASCII character to integer (digit)
-    imul eax, eax, 10   ; Multiply the current result by 10
-    add eax, edx        ; Add the new digit to the result
-    inc ecx             ; Move to the next character
-    jmp .loop           ; Repeat the loop
-.done:
-    ret
-
-; Function to convert an integer to an ASCII string
 int_to_ascii:
-    add esi, dword [len] ; Point esi to the end of the buffer
-    mov byte [esi], 0   ; Null-terminate the string
-.reverseLoop:
-    dec esi             ; Move back through the buffer
-    mov ebx, 10         ; Divisor (to convert to ASCII digits)
-    xor ecx, ecx        ; Clear ecx (loop counter)
-.nextDigit:
-    xor edx, edx        ; Clear edx to get the remainder
-    div ebx             ; Divide eax by 10, result in eax, remainder in edx
-    add dl, '0'         ; Convert the remainder to ASCII
-    mov [esi], dl       ; Store the ASCII digit in the buffer
-    inc ecx             ; Move to the next position in the buffer
-    test eax, eax       ; Check if quotient is zero (end of conversion)
-    jnz .nextDigit      ; If not zero, continue the loop
-    mov dword [len], ecx ; Store the length of the ASCII string
-    ret
+    ; Convert the integer (in [num]) to ASCII string (output in [ebx])
+    xor ecx, ecx             ; Clear ecx (used for digit count)
+
+    check_zero:
+        cmp dword [num], 0   ; Check if the number is zero
+        jz end_conversion    ; If it's zero, end the conversion
+
+        mov eax, 10          ; Divide by 10 (since we're working with 32-bit integers)
+        div eax              ; Divide [num] by 10, quotient in eax, remainder in edx
+
+        add dl, '0'          ; Convert remainder to ASCII character
+        dec ebx              ; Move back one position in the buffer (starting from the end)
+        mov [ebx], dl        ; Store the ASCII character in the buffer
+        inc ecx              ; Increment digit count
+
+        jmp check_zero      ; Repeat the loop to handle next digit
+
+    end_conversion:
+        test ecx, ecx       ; Check if the number was zero (no digits)
+        jnz not_zero        ; If it's not zero, skip this part
+
+        ; Special case for 0 (single-digit number)
+        mov byte [ebx], '0' ; Store '0' in the buffer
+        inc ecx             ; Increment digit count
+
+    not_zero:
+        mov byte [ebx+ecx], 0 ; Null-terminate the string
+        ret
