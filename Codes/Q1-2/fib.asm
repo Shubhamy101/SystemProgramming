6b46346f07909967d7a48e53f3d3ac252783bdf2
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-25 22:43:12 +0530

    Update fib.asm

--- Codes/Q1-2/fib.asm
+++ Codes/Q1-2/fib.asm
@@ -45,35 +45,35 @@ _start:
     mov eax, 4        ; syscall for sys_write
     mov ebx, 1        ; file descriptor 1 (stdout)
     mov ecx, out_msg  ; pointer to the output message
-    mov edx, 30       ; message length (adjust as needed)
+    mov edx, out_msg_len
     int 0x80          ; make syscall
 
     ; Convert the result (F(n)) to a string
     mov eax, ecx      ; Copy the Fibonacci number to EAX (result in ECX)
     mov edi, 10       ; Divisor (10)
     xor esi, esi      ; Counter for the number of digits in the Fibonacci number
-    mov edx, 0        ; Clear EDX for the division operation
+    xor ecx, ecx      ; Clear ECX before storing digits
 
 .convert_to_string:
     xor edx, edx      ; Clear EDX before the division
     div edi           ; Divide EAX by 10, quotient in EAX, remainder in EDX
-    push edx          ; Store the remainder (digit) on the stack
+    add dl, '0'       ; Convert the digit to ASCII character
+    mov [fib + esi], dl ; Store the ASCII digit in the fib buffer
     inc esi           ; Increment the digit counter
     test eax, eax     ; Check if EAX is zero (F(n) has been completely converted)
     jnz .convert_to_string
 
     ; Display the Fibonacci number digit by digit
 .display_loop:
-    pop edx           ; Pop the next digit from the stack
-    add dl, '0'       ; Convert the digit to ASCII character
+    dec esi           ; Move the digit counter back to the last digit
     mov eax, 4        ; syscall for sys_write
     mov ebx, 1        ; file descriptor 1 (stdout)
-    mov ecx, edx      ; pointer to the digit character
+    lea ecx, [fib + esi] ; pointer to the digit character
     mov edx, 1        ; number of bytes to write (1 character)
     int 0x80          ; make syscall
 
     ; Continue displaying the digits until all are printed
-    dec esi
+    test esi, esi
     jnz .display_loop
 
     ; Display a newline character
