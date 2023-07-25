d75195a9a9e7f7f9ffca81b2f63ec6b0f3f32eeb
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-26 02:52:02 +0530

    Update fib.asm

--- Codes/Q1-2/fib.asm
+++ Codes/Q1-2/fib.asm
@@ -1,102 +1,42 @@
-section .data
-    out_msg db "The nth Fibonacci number is: ", 0
-    out_msg_len equ $ - out_msg
+%include"io.mac"
 
-    nl db 10  ; Define the newline character
+section .data					;messages to be printed
+input db "Enter the number",0
+output db "th fibonocci number is ",0
 
-section .text
-    global _start
+section .bss					;variable declaration section
+number resd 1
+temp resd 1
 
+section .text					;main program starts here 
+global _start
 _start:
-    ; Programmer-defined constant for the value of n
-    mov eax, 10  ; Change this value to find the nth Fibonacci number
+	PutStr input
+	GetLInt ecx
+	mov [number],ecx
+	mov eax,1
+	mov ebx,1
+	call fibo				;calls fibonocci number
 
-    ; Check if n is 0 or 1 (special cases)
-    cmp eax, 0
-    je .fibonacci_done
-    cmp eax, 1
-    je .fibonacci_done
+print:						;prints the nthfibonocci number
+	PutLInt [number]
+	PutStr output
+	PutLInt eax
+	nwln
 
-    ; Initialize variables to hold the last two Fibonacci numbers
-    mov ebx, 0  ; F(n-2)
-    mov ecx, 1  ; F(n-1)
+done:						;exits out of the program
+	.EXIT
 
-    ; Display the output message
-    mov eax, 4        ; syscall for sys_write
-    mov ebx, 1        ; file descriptor 1 (stdout)
-    mov ecx, out_msg  ; pointer to the output message
-    mov edx, out_msg_len
-    int 0x80          ; make syscall
+fibo:						;comes here when the fibo label is called		
+	cmp ecx,2				;compares if the number we needed reached or not
+	jg fibo1
+	ret
 
-    ; Loop to calculate the nth Fibonacci number
-.fibonacci_loop:
-    ; Calculate F(n) = F(n-1) + F(n-2)
-    add ecx, ebx
+fibo1:						;Logic for fibonocci number
+	mov [temp],eax
+	add eax,ebx
+	mov ebx,[temp]
+	sub ecx,1
+	jmp fibo				;jumps back to fibonocci after the loop is executed
 
-    ; Update variables for the next iteration
-    xchg ecx, ebx  ; F(n-2) = F(n-1), F(n-1) = F(n)
-
-    ; Decrement the counter
-    dec eax
-
-    ; Display the current Fibonacci number
-    push eax
-    call display_number  ; Call the display_number function to print the Fibonacci number
-    pop eax
-
-    ; Check if we have reached the last Fibonacci number (F(n))
-    jnz .fibonacci_loop
-
-.fibonacci_done:
-    ; Display a newline character
-    mov eax, 4        ; syscall for sys_write
-    mov ebx, 1        ; file descriptor 1 (stdout)
-    mov ecx, nl       ; pointer to the newline character
-    mov edx, 1        ; number of bytes to write (1 character)
-    int 0x80          ; make syscall
-
-    ; Exit the program
-    mov eax, 1        ; syscall for sys_exit
-    xor ebx, ebx      ; exit code 0
-    int 0x80          ; make syscall
-
-display_number:
-    ; Clear the buffer before storing the ASCII representation of the number
-    mov edi, fib      ; Set EDI to point to the buffer (fib)
-    xor eax, eax      ; Clear EAX (used as a flag to indicate leading zeros)
-    mov ecx, 10       ; Set ECX to 10 (number of digits to display)
-.clear_buffer_loop:
-    mov byte [edi], '0'  ; Clear the byte at each position in the buffer
-    inc edi
-    loop .clear_buffer_loop
-
-    ; Convert the result (F(n)) to a string and store it in the buffer (fib)
-    mov eax, ecx     ; Restore ECX (used as a loop counter)
-    mov esi, 10      ; Set ESI to 10 (used as a divisor)
-.convert_to_string:
-    xor edx, edx     ; Clear EDX before the division
-    div esi          ; Divide EAX by 10, quotient in EAX, remainder in EDX
-    add dl, '0'      ; Convert the digit to ASCII character
-    dec edi          ; Move to the previous position in the buffer
-    mov byte [edi], dl  ; Store the ASCII digit in the fib buffer
-    test eax, eax    ; Check if EAX is zero (F(n) has been completely converted)
-    jnz .convert_to_string
-
-    ; Display the Fibonacci number digit by digit
-.display_loop:
-    mov eax, 4        ; syscall for sys_write
-    mov ebx, 1        ; file descriptor 1 (stdout)
-    mov ecx, edi      ; pointer to the digit character
-    mov edx, 1        ; number of bytes to write (1 character)
-    int 0x80          ; make syscall
-
-    ; Move to the previous position in the buffer
-    inc edi
-    cmp byte [edi], '0'  ; Check if the byte in the buffer is not '0'
-    jnz .display_loop
-
-    ; Return from the display_number function
-    ret
-
-section .bss
-    fib resb 10  ; Buffer to store the ASCII representation of the Fibonacci number
+	
