48803869a619ef41a37be979454449c3fc884f5f
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-26 00:52:42 +0530

    Update test.asm

--- Codes/Q1-2/test.asm
+++ Codes/Q1-2/test.asm
@@ -1,22 +1,22 @@
-section .text
-    default rel
-    extern printf
-    global main
-main:
-    ; Create a stack-frame, re-aligning the stack to 16-byte alignment before calls
-    push rbp
-
-    mov	rdi, fmt
-    mov	rsi, message
-    mov	rax, 0
-
-    ; Call printf
-    call printf wrt ..plt
-    
-    pop	rbp		; Pop stack
-
-    mov	rax,0	; Exit code 0
-    ret			; Return
 section .data
-    message:  db        "Hello, World", 10, 0
-    fmt:    db "%s", 10, 0
+    num1 dq 10         ; First number (change this to any desired value)
+    num2 dq 20         ; Second number (change this to any desired value)
+    fmt_result db "Sum: %d", 10, 0
+
+section .text
+    global _start
+
+_start:
+    ; Calculate the sum
+    mov rax, qword [num1]
+    add rax, qword [num2]
+
+    ; Print the sum
+    mov rdi, fmt_result
+    mov rsi, rax        ; The sum is stored in rax
+    call printf wrt ..plt
+
+    ; Exit the program
+    mov rax, 60         ; System call number for sys_exit (60)
+    xor rdi, rdi        ; Exit code 0
+    syscall
