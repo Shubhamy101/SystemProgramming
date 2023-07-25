f0cff8f1242161b8ecb83202d1593d7d5543a2e5
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-26 01:29:22 +0530

    Update itoa.asm

--- Codes/Q1-2/itoa.asm
+++ Codes/Q1-2/itoa.asm
@@ -1,14 +1,37 @@
 section	.text
    global _start        ;must be declared for using gcc
-	
-_start:	                ;tell linker entry point
-   mov	eax,'3'
-   sub   eax, '0'
-	
-   mov 	ebx, '4'
-   sub     ebx, '0'
-   add 	eax, ebx
-   add	eax, '0'
+
+_start:
+    ; Set n to the desired value (e.g., n = 10)
+    mov  ebx,10
+
+    ; Check for base cases (n = 0 and n = 1)
+    cmp ebx, 0
+    je fibonacci_done
+    cmp ebx, 1
+    je fibonacci_done
+    jmp start_iteration
+
+start_iteration:
+    ; Initialize the first two Fibonacci numbers (F0 = 0, F1 = 1)
+    mov dword [fib_number], 0   ; F0
+    mov eax, 1                  ; F1
+
+    ; Loop to calculate the Fibonacci number iteratively
+    mov ecx, ebx        ; ECX will act as a loop counter
+    dec ecx             ; We already have F0 and F1 calculated, so we need to calculate (n - 1) iterations
+
+fibonacci_loop:
+    add eax, dword [fib_number] ; Calculate the next Fibonacci number (F2 = F0 + F1)
+    xchg eax, dword [fib_number] ; Store the new Fibonacci number and move the previous one to EAX
+
+    loop fibonacci_loop ; Decrement ECX and loop until ECX becomes zero
+
+fibonacci_done:
+    ; At this point, the nth Fibonacci number will be stored in EAX
+
+    ; Add your code here to use/display the result as needed.
+    ; For example, you can print the Fibonacci number using syscalls for Linux.
 	
    mov 	[sum], eax
    mov	ecx,msg	
