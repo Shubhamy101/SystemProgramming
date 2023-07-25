29fdf0e2bf254b2318b49c29ae62f89a40b558d1
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-26 00:25:14 +0530

    Update itoa.asm

--- Codes/Q1-2/itoa.asm
+++ Codes/Q1-2/itoa.asm
@@ -10,22 +10,22 @@ section .text
 
 _start:
     ; Read the integer from the command-line argument
-    mov rdi, 1                   ; File descriptor 1 points to stdout (standard output)
-    mov rsi, rdx                 ; rdx register contains the pointer to the arguments array
-    mov rdx, 10                  ; Read up to 10 bytes (we assume the number won't exceed 10 digits)
+    mov edi, 1                   ; File descriptor 1 points to stdout (standard output)
+    mov esi, rdx                 ; rdx register contains the pointer to the arguments array
+    mov edx, 10                  ; Read up to 10 bytes (we assume the number won't exceed 10 digits)
     call read_int
 
     ; Convert the integer to ASCII
-    mov rdi, buffer              ; Destination buffer for the ASCII string
+    mov edi, buffer              ; Destination buffer for the ASCII string
     call int_to_ascii
 
     ; Print the result
-    mov rsi, rdi                 ; rsi holds the pointer to the ASCII string
+    mov esi, edi                 ; rsi holds the pointer to the ASCII string
     call print_string
 
     ; Exit the program
-    mov rax, 60                  ; syscall number for exit
-    xor rdi, rdi                 ; Exit code 0
+    mov eax, 60                  ; syscall number for exit
+    xor edi, edi                 ; Exit code 0
     syscall
 
 ; Function to convert an integer to an ASCII string
@@ -33,33 +33,33 @@ _start:
 ;   rdi: Destination buffer for the ASCII string
 ;   rsi: Input integer
 int_to_ascii:
-    mov rcx, 10                 ; Set divisor (base 10)
-    mov rax, rsi                ; Copy the integer to rax
-    mov rbx, rdi                ; Copy the buffer address to rbx
+    mov ecx, 10                 ; Set divisor (base 10)
+    mov eax, esi                ; Copy the integer to rax
+    mov ebx, edi                ; Copy the buffer address to rbx
 
-    add rbx, 10                 ; Move the buffer pointer to the end (for null terminator)
+    add ebx, 10                 ; Move the buffer pointer to the end (for null terminator)
 
-    mov byte [rbx], 0           ; Null-terminate the string
+    mov byte [ebx], 0           ; Null-terminate the string
 
 int_to_ascii_loop:
-    dec rbx                     ; Move the buffer pointer back
-    xor rdx, rdx                ; Clear rdx for division
-    div rcx                     ; Divide rax by 10, quotient in rax, remainder in rdx
+    dec ebx                     ; Move the buffer pointer back
+    xor edx, edx                ; Clear rdx for division
+    div ecx                     ; Divide rax by 10, quotient in rax, remainder in rdx
     add dl, '0'                 ; Convert the remainder to ASCII
-    mov [rbx], dl               ; Store the ASCII character in the buffer
+    mov [ebx], dl               ; Store the ASCII character in the buffer
 
-    test rax, rax               ; Check if quotient is zero
+    test eax, eax               ; Check if quotient is zero
     jnz int_to_ascii_loop       ; If not, continue the loop
 
-    mov rdi, rbx                ; Set rdi to point to the beginning of the ASCII string
+    mov edi, ebx                ; Set rdi to point to the beginning of the ASCII string
     ret
 
 ; Function to print a null-terminated string
 ; Input:
 ;   rdi: Pointer to the null-terminated string to be printed
 print_string:
-    mov rax, 0x1               ; syscall number for write
-    mov rdx, 0xFFFFFFFF        ; Set the maximum number of bytes to write (large enough)
-    xor rsi, rsi               ; rsi = 0, as we start writing from the beginning of the string
+    mov eax, 0x1               ; syscall number for write
+    mov edx, 0xFFFFFFFF        ; Set the maximum number of bytes to write (large enough)
+    xor esi, esi               ; rsi = 0, as we start writing from the beginning of the string
     syscall
     ret
