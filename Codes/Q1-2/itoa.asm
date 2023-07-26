561a3ed58a29d453e8a4300e2f561e7dfce1ca4f
Shubham Yadav <50779707+Shubhamy101@users.noreply.github.com>
2023-07-27 01:09:53 +0530

    Update itoa.asm

--- Codes/Q1-2/itoa.asm
+++ Codes/Q1-2/itoa.asm
@@ -1,57 +1,59 @@
-section	.text
-   global _start        ;must be declared for using gcc
-
+section .bss
+    digitSpace resb 100
+    digitSpacePos resb 8
+ 
+section .text
+    global _start
+ 
 _start:
-    ; Set n to the desired value (e.g., n = 10)
-    mov  ebx,10
-
-    ; Check for base cases (n = 0 and n = 1)
-    cmp ebx, 0
-    je fibonacci_done
-    cmp ebx, 1
-    je fibonacci_done
-    jmp start_iteration
-
-start_iteration:
-    ; Initialize the first two Fibonacci numbers (F0 = 0, F1 = 1)
-    mov dword [sum], 0   ; F0
-    mov eax, 1                  ; F1
-
-    ; Loop to calculate the Fibonacci number iteratively
-    mov ecx, ebx        ; ECX will act as a loop counter
-    dec ecx             ; We already have F0 and F1 calculated, so we need to calculate (n - 1) iterations
-
-fibonacci_loop:
-    add eax, dword [sum] ; Calculate the next Fibonacci number (F2 = F0 + F1)
-    xchg eax, dword [sum] ; Store the new Fibonacci number and move the previous one to EAX
-
-    loop fibonacci_loop ; Decrement ECX and loop until ECX becomes zero
-
-fibonacci_done:
-    ; At this point, the nth Fibonacci number will be stored in EAX
-
-    ; Add your code here to use/display the result as needed.
-    ; For example, you can print the Fibonacci number using syscalls for Linux.
-	
-   mov 	[sum], eax
-   mov	ecx,msg	
-   mov	edx, len
-   mov	ebx,1	         ;file descriptor (stdout)
-   mov	eax,4	         ;system call number (sys_write)
-   int	0x80	         ;call kernel
-	
-   mov	ecx,sum
-   mov	edx, 1
-   mov	ebx,1	         ;file descriptor (stdout)
-   mov	eax,4	         ;system call number (sys_write)
-   int	0x80	         ;call kernel
-	
-   mov	eax,1	         ;system call number (sys_exit)
-   int	0x80	         ;call kernel
-	
-section .data
-msg db "The nth fibonacci number is:", 0xA,0xD 
-len equ $ - msg   
-segment .bss
-
-sum resb 4
+ 
+    mov rax, 12345     ; Replace '12345' with the integer you want to convert to ASCII
+    call _intToAscii    ; Convert the integer in rax to ASCII
+ 
+    ; Your code to print the ASCII string goes here
+    ; (For example, you can use 'write' syscall to print the ASCII string)
+ 
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+ 
+_intToAscii:
+    xor rcx, rcx       ; Clear rcx to use it as a counter for the digits
+    mov rbx, 10        ; Base 10 to divide the number
+ 
+_intToAsciiLoop:
+    xor rdx, rdx       ; Clear rdx for division
+    div rbx            ; Divide rax by 10, quotient in rax, remainder in rdx
+    add dl, '0'        ; Convert the remainder to ASCII character
+ 
+    ; Store the ASCII character in the digitSpace buffer in reverse order
+    mov [digitSpace + rcx], dl
+ 
+    inc rcx            ; Increment the counter
+ 
+    test rax, rax      ; Check if quotient is zero (end of conversion)
+    jnz _intToAsciiLoop
+ 
+    ; Null-terminate the string
+    mov byte [digitSpace + rcx], 0
+ 
+    ; Calculate the length of the ASCII string (excluding null terminator)
+    mov [digitSpacePos], rcx
+ 
+    ; Reverse the ASCII string in place to get the correct order
+    xor rsi, rsi       ; rsi points to the beginning of the string
+_reverseLoop:
+    mov rdi, rcx       ; rdi points to the end of the string
+    dec rdi
+    cmp rsi, rdi
+    jge _reverseDone   ; If rsi >= rdi, the entire string is reversed
+    mov al, [digitSpace + rsi]
+    mov dl, [digitSpace + rdi]
+    mov [digitSpace + rsi], dl
+    mov [digitSpace + rdi], al
+    inc rsi
+    dec rdi
+    jmp _reverseLoop
+ 
+_reverseDone:
+    ret
